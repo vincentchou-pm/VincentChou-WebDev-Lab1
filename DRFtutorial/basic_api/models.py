80b532bcab73a93b1feb16f56964b95290531d17
vincentchou-pm <23502410025@student.prasetiyamulya.ac.id>
2025-11-02 17:49:37 +0700

    added search, cover, filter feature

---
+++ DRFtutorial/basic_api/models.py
@@ -0,0 +1,23 @@
+# basic_api/models.py
+from django.db import models
+
+#list
+Grade = [
+    ('excellent', 1),
+    ('average', 0),
+    ('bad', -1)
+]
+
+# DataFlair
+class DRFPost(models.Model):
+    name = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 100)
+    uploaded = models.DateTimeField(auto_now_add = True)
+    rating = models.CharField(choices = Grade, default = 'average', max_length = 50)
+    cover = models.ImageField(upload_to = 'images/', null = True, blank = True)
+
+    class Meta:
+        ordering = ['uploaded']
+
+    def __str__(self):
+        return self.name
